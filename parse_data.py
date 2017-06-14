802545fe4dac5c153b42885e64a3f1916a2ebf8c
Karen Hambardzumyan <mahnerak@yerevann.com>
2017-06-14 16:31:17 +0400

    Train/Valid set splitting

--- parse_data.py
+++ parse_data.py
@@ -1,14 +1,23 @@
 """
-Usage: python parse_data.py dataset_file outfile
+Usage: python parse_data.py dataset_file --output_file outfile.json --train_ratio 1.
 """
 # -*- coding: utf-8 -*-
 import json
-import sys
+import argparse
+import random
+random.seed(20)
 
-file_path = sys.argv[1]
-outfile = 'tmp.json'
-if (len(sys.argv) > 2):
-    outfile = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument('data', help='Path to the dataset file', type=str)
+parser.add_argument('--output_destination', default='data/tmp.json',
+                    help='Desired path to output json', type=str)
+parser.add_argument('--train_ratio', default=1., help='ratio for train/val split', type=float)
+args = parser.parse_args()
+
+
+file_path = args.data
+outfile = args.output_destination
+train_ratio = args.train_ratio
 
 json_data = open(file_path, 'r').read()
 data = json.loads(json_data)
@@ -27,10 +36,15 @@ print "Each paragraph has some questions and answers associated with it"
 print "Keys of qas sections are:", data[0]['paragraphs'][0]['qas'][0].keys()
 print "Keys of answers are:", data[0]['paragraphs'][0]['qas'][0]['answers'][0].keys()
 
-cqas = [] # ContextQuestionAnswer
+train_cqas = [] # ContextQuestionAnswer
+val_cqas = []
 
 for topic_id in range(len(data)):
     paragraphs = data[topic_id]['paragraphs']
+    if random.random() < train_ratio:
+        train = True
+    else:
+        train = False
     for paragraph in paragraphs:
         context = paragraph['context']
         for qa in paragraph['qas']:
@@ -40,32 +54,28 @@ for topic_id in range(len(data)):
             _id = qa['id']
             answer = qa['answers'][0]['text']
             answer_start = qa['answers'][0]['answer_start'] 
+            answer_end = answer_start + len(answer) - 1 # answer == context[answer_start : answer_end + 1]
+            if train:
+                train_cqas.append({"context": context, "question": question, "answer": answer,
+                    'answer_start': answer_start, 'answer_end': answer_end,
+                    'id': _id, 'topic': topics[topic_id]
+                })
+            else:
+                val_cqas.append({"context": context, "question": question, "answer": answer,
+                    'answer_start': answer_start, 'answer_end': answer_end,
+                    'id': _id, 'topic': topics[topic_id]
+                })
             
-            cqas.append({"context": context, "question": question, "answer": answer,
-                'answer_start': answer_start, 'id': _id, 'topic': topics[topic_id]
-            })
-            
-print "Dataset contains %d (context, question, answer) triples" % len(cqas)
-
-cnt = [0 for i in range(100)]
-max_cnt = 0
-for cqa in cqas:
-    answer = cqa['answer']
-    words = len(answer.split(' '))
-    cnt[words] += 1
-    max_cnt = max(max_cnt, words)
-
-print "answer length distribution:"
-for i in range(max_cnt + 1):
-    print "%d: %d" % (i, cnt[i])
-
-
-print "answer length coverage distribution:"
-s = 0    
-for i in range(max_cnt + 1):
-    s += cnt[i]
-    print "%d: %f" % (i, float(s) / len(cqas))
 
 print "Saving dataset to outfile..."
-with open(outfile, 'w') as out:
-    json.dump(cqas, out)
+if train_ratio == 1.:
+    with open(outfile, 'w') as fd:
+        json.dump(train_cqas,fd)
+else:
+    print "Train/Val ratio is %f" % (1. * len(train_cqas) / len(val_cqas))
+    train_file = 'train_' + outfile
+    val_file = 'val_' + outfile
+    with open(train_file, 'w') as fd:
+        json.dump(train_cqas, fd)
+    with open(val_file, 'w') as fd:
+        json.dump(val_cqas, fd)
